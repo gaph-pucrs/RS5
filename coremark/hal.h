04655502cc2c02e63929ed81ea3a2b2a6525ef3f
Willian-Nunes <willian-anunes@hotmail.com>
2021-12-08 12:36:38 -0300

    added coremark files

---
+++ coremark/hal.h
@@ -0,0 +1,14 @@
+#ifndef _HAL_H
+#define _HAL_H
+
+#include "prototypes.h"
+
+#define TIMER_REG (*(volatile uint32_t*)0x80006000)
+#define UART_REG (*(volatile char*)0x80004000)
+
+// Stubs, need real implementation
+static inline int enter_critical() { return 0; }
+static inline void leave_critical(int i) {}
+void halt() __attribute__ ((noreturn));
+
+#endif
