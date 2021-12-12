44a8fff3c84b40cee4752eed598dfa8e617d05a3
Willian-Nunes <willian-anunes@hotmail.com>
2021-12-12 13:26:34 -0300

    changed some file names

---
+++ app/samplecode/bareOS/crt0.s
@@ -0,0 +1,16 @@
+	.section .text.init
+	.global _entry
+_entry:
+	la a0, _bss_start
+	la a1, _bss_end
+	la sp, _stack
+	la gp, _gp
+.clr_bss:
+	bgeu a0, a1, .clr_bss_done
+	sw zero, 0(a0)
+	addi a0, a0, 4
+	beq zero, zero, .clr_bss
+.clr_bss_done:
+	la ra, halt
+	j kmain
+
