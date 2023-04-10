5f343cd36d1b30dce197258e95bc2a363c2769c2
Caroline Borges <caroline.s@edu.pucrs.br>
2023-04-10 15:13:08 -0300

    Add RTC rtl

--- rtl/rtc.sv
+++ rtl/rtc.sv
@@ -1,50 +1,73 @@
 module rtc (
-    input         clk,
-    input         reset,
-    input  [3:0]  addr,
-    //input  [7:0]  we,
-    input  [63:0] data_i,
-    output [63:0] data_o,
-    //output [63:0] mtime_o,
-    //output [63:0] mtimecmp_o
+    input  logic        clk,
+    input  logic        reset,
+    input  logic [3:0]  addr_i,
+    input  logic [7:0]  we_i,
+    input  logic [63:0] data_i,
+    output logic [63:0] data_o,
+    output logic        mti_o,
+    output logic [63:0] mtime_o
 );
-    logic mtime [63:0];
-    logic mtimecmp [63:0];
+    // memory[7:0] = mtime (64 bits)
+    // memory[15:0] = mtimecmp (64 bits)
+    logic [15:0][7:0] memory;
+    logic intr;
+
+    assign intr = (memory[7:0] >= memory[15:8]);
+    assign mtime_o = memory[7:0];
 
     always_ff @(posedge clk) begin
         if (reset) begin
-            mtime    <= '0;
-            mtimecmp <= '0;
-        end 
+            mti_o <= 1'b0;
+        end
         else begin
-            mtime <= mtime + 1;
-            if (mtime >= mtimecmp) begin
-            // Interrupt generation
-            // .
-            // .
-            // .
-            end
+            mti_o <= intr;
         end
     end
 
     always_ff @(posedge clk) begin
-    if (!reset) begin
-        ///////////////////////////// Writes  ///////////////////////////////////////////
-        if (addr != '0) begin
-            if (addr[3]) begin                                
-                // data_o[63:47] <= data_i[63:47];
-            end 
-            if (addr[2]) begin                                 
-               // data_i[47:31];
+        if (reset) begin
+            memory <= '0;
+        end 
+        else begin
+            memory[7:0] <= memory[7:0] + 1'b1;
+            if (we_i != '0) begin
+                if (we_i[7]) begin                                 
+                    memory[addr_i+7] <= data_i[63:56];
+                end 
+                if (we_i[6]) begin                                 
+                    memory[addr_i+6] <= data_i[55:48];
+                end
+                if (we_i[5]) begin                                 
+                    memory[addr_i+5] <= data_i[47:40];
+                end
+                if (we_i[4]) begin                                 
+                    memory[addr_i+4] <= data_i[39:32];
+                end
+                if (we_i[3]) begin                                
+                    memory[addr_i+3] <= data_i[31:24];
+                end 
+                if (we_i[2]) begin                                 
+                    memory[addr_i+2] <= data_i[23:16];
+                end
+                if (we_i[1]) begin                                 
+                    memory[addr_i+1] <= data_i[15:8];
+                end
+                if (we_i[0]) begin                                 
+                    memory[addr_i]   <= data_i[7:0];
+                end
             end
-            if (addr[1]) begin                                 
-                // data_i[31:15];
-            end
-            if (addr[0]) begin                                 
-                // data_i[15:0];
+            else begin
+                data_o[63:56] <= memory[addr_i+7];
+                data_o[55:48] <= memory[addr_i+6];
+                data_o[47:40] <= memory[addr_i+5];
+                data_o[39:32] <= memory[addr_i+4];
+                data_o[31:24] <= memory[addr_i+3];
+                data_o[23:16] <= memory[addr_i+2];
+                data_o[15:8]  <= memory[addr_i+1];
+                data_o[7:0]   <= memory[addr_i];
             end
         end
-    end
     end
 
 endmodule
