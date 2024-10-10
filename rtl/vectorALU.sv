d47b784513e6959be62657ccb61a4a7b96392db7
Antônio dos Santos <antonio.s001@edu.pucrs.br>
2024-10-10 16:01:30 -0300

    added V_MERGE enable

--- rtl/vectorALU.sv
+++ rtl/vectorALU.sv
@@ -12,6 +12,7 @@ module vectorALU
     parameter int V_REDLOGIC_ON   = 1'b1,
     parameter int V_REDMINMAX_ON  = 1'b1,
     parameter int V_REDSUM_ON     = 1'b1,
+    parameter int V_MERGE_ON      = 1'b1,
     parameter int V_DIV_ON        = 1'b1
 ) (
     input  logic                        clk,
@@ -1340,28 +1341,33 @@ module vectorALU
 //  Merge
 //////////////////////////////////////////////////////////////////////////////
 
+    
     logic [(VLEN-1):0] result_merge;
     
-    always_comb begin
-        unique case (vsew)
-            EW8: begin
-                for (int i = 0; i < VLENB; i++) begin
-                    result_merge[(8*i)+:8]    = (mask_sew8[cycle_count_r][i])  ? second_operand_8b[i]  : first_operand_8b[i];
-                end 
-            end
-            EW16: begin
-                for (int i = 0; i < (VLENB/2); i++) begin
-                    result_merge[(16*i)+:16]  = (mask_sew16[cycle_count_r][i]) ? second_operand_16b[i] : first_operand_16b[i];
-                end 
-            end
-            default: begin
-                for (int i = 0; i < (VLENB/4); i++) begin
-                    result_merge[(32*i)+:32]  = (mask_sew32[cycle_count_r][i]) ? second_operand_32b[i] : first_operand_32b[i];
-                end 
-            end
-        endcase
+    if(V_MERGE_ON) begin : v_merge_gen_on
+        always_comb begin
+            unique case (vsew)
+                EW8: begin
+                    for (int i = 0; i < VLENB; i++) begin
+                        result_merge[(8*i)+:8]    = (mask_sew8[cycle_count_r][i])  ? second_operand_8b[i]  : first_operand_8b[i];
+                    end 
+                end
+                EW16: begin
+                    for (int i = 0; i < (VLENB/2); i++) begin
+                        result_merge[(16*i)+:16]  = (mask_sew16[cycle_count_r][i]) ? second_operand_16b[i] : first_operand_16b[i];
+                    end 
+                end
+                default: begin
+                    for (int i = 0; i < (VLENB/4); i++) begin
+                        result_merge[(32*i)+:32]  = (mask_sew32[cycle_count_r][i]) ? second_operand_32b[i] : first_operand_32b[i];
+                    end 
+                end
+            endcase
+        end
     end
-
+    else begin : v_merge_gen_off
+        assign result_merge = '0;
+    end
 
 //////////////////////////////////////////////////////////////////////////////
 // Result Demux
