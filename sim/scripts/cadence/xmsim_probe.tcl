fa7edca9268a40773759aee9a3928177736b44aa
Carlos Gewehr <cggewehr@gmail.com>
2025-07-10 02:30:09 -0300

    Start and exit xcelium simulation automatically

--- sim/scripts/cadence/xmsim_probe.tcl
+++ sim/scripts/cadence/xmsim_probe.tcl
@@ -1,3 +1,4 @@
-
 database -open -shm -into waves.shm waves -default
-probe -create -database waves -all -memories -depth all testbench.dut
+probe -create -database waves -all -memories -depth all testbench.dut testbench.u_rvfi_monitor
+run
+exit
