e369309e2df335a263b6a9e23dc5eef9346a2429
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2025-10-11 17:37:32 -0300

    Rework base for automatic jump latency and add a base for BP

--- rtl/fetch.sv
+++ rtl/fetch.sv
@@ -24,9 +24,9 @@ module fetch
     import RS5_pkg::*;
 #(
     parameter       start_address = 32'b0,
+    parameter bit   BRANCHPRED = 1'b1, 
     /* verilator lint_off UNUSEDPARAM */
     parameter bit   COMPRESSED = 1'b0,
-    parameter bit   BRANCHPRED = 1'b1,    
     parameter mul_e MULEXT     = MUL_M,
     parameter bit   ZCBEnable  = 1'b0
     /* verilator lint_on UNUSEDPARAM */
@@ -36,33 +36,34 @@ module fetch
     input   logic           reset_n,
     input   logic           sys_reset,
     input   logic           enable_i,
-    input   logic           busy_i,
-    output  logic           valid_o,
-    output  logic           bp_ack_o,
 
+    /* Jump control */
     input   logic           jump_i,
     input   logic           ctx_switch_i,
-    /* Bits 1:0 unused without compressed */
-    /* verilator lint_off UNUSEDSIGNAL */
     input   logic [31:0]    jump_target_i,
     input   logic [31:0]    ctx_switch_target_i,
-    /* verilator lint_on UNUSEDSIGNAL */
     output  logic           jumping_o,
+    output  logic           jump_misaligned_o,
     
-    /* Bits 1:0 unused without BP */
+    /* Branch prediction */
     /* verilator lint_off UNUSEDSIGNAL */
     input   logic           bp_take_i,
     input   logic [31:0]    bp_target_i,
-    input   logic           jump_rollback_i,
     /* verilator lint_on UNUSEDSIGNAL */
+    input   logic           jump_rollback_i,
     output  logic           bp_rollback_o,
+    output  logic           bp_ack_o,
 
-    output  logic           jump_misaligned_o,
-    output  logic           compressed_o,
+    /* Memory interface */
+    input   logic           busy_i,
     output  logic [31:0]    instruction_address_o,
     input   logic [31:0]    instruction_data_i,
+
+    /* Fetched output */
+    output  logic [31:0]    pc_o,
     output  logic [31:0]    instruction_o,
-    output  logic [31:0]    pc_o
+    output  logic           valid_o,
+    output  logic           compressed_o
 );
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -70,10 +71,14 @@ module fetch
 ////////////////////////////////////////////////////////////////////////////////
 
     logic jumped;
-    assign jumped = ctx_switch_i || jump_i;
+    logic jump_confirmed;
+    logic valid;
+    logic iaddr_hold;
+    logic jumped_r;
+    logic [31:0] instruction_address;
 
     logic empty_buffer;
-    assign empty_buffer = sys_reset || jumped;
+    assign empty_buffer = sys_reset || jump_confirmed;
 
     logic valid_buffer;
 
@@ -88,7 +93,6 @@ module fetch
     end
 
     logic push;
-    assign push = !busy_r && !(enable_i && !valid_buffer);
 
     /* verilator lint_off UNUSEDSIGNAL */
     logic mem_operation_en;
@@ -105,7 +109,7 @@ module fetch
     RingBuffer #(
         .DATA_SIZE(32),
         .BUFFER_SIZE(4),
-        .ALMOST_FULL_THRESHOLD(1),
+        .ALMOST_FULL_THRESHOLD(1), /* DO NOT CHANGE */
         .ALMOST_EMPTY_THRESHOLD(0)
     ) ibuf (
         .clk_i         (clk),
@@ -122,15 +126,85 @@ module fetch
     );
 
     logic [31:0] iaddr_jumped;
-    always_comb begin
-        if (ctx_switch_i)
-            iaddr_jumped = ctx_switch_target_i;
-        else
-            iaddr_jumped = jump_target_i;
+    logic [31:0] next_instruction_address;
+
+    logic jump_rollback_r;
+    always_ff @(posedge clk or negedge reset_n) begin
+        if (!reset_n) begin
+            jump_rollback_r <= 1'b0;
+        end
+        else if (sys_reset) begin
+            jump_rollback_r <= 1'b0;
+        end
+        else begin 
+            // if (jump_rollback_i)
+            //     jump_rollback_r <= 1'b1;
+            // // else if (!busy_i)
+            // else if (valid_o)
+            // // else
+            //     jump_rollback_r <= 1'b0;
+            jump_rollback_r <= jump_rollback_i;
+        end
     end
 
-    logic iaddr_hold;
-    assign iaddr_hold = (almost_full && !pop) || busy_i;
+    logic valid_fetch;
+    assign valid_fetch = !busy_r && !jump_rollback_r && !(jumped_r && !jump_rollback_i);
+
+    if (BRANCHPRED) begin : gen_bp_on
+        logic bp_taken;
+        // assign bp_taken = (enable_i && bp_take_i && valid && push);
+        assign bp_taken       = (enable_i && bp_take_i && valid);
+        assign jumped         = ctx_switch_i || jump_i || bp_taken;
+        assign jump_confirmed = ctx_switch_i || jump_i || (bp_ack_o && !jump_rollback_i);
+
+        logic [31:0] iaddr_rollback;
+        always_ff @(posedge clk or negedge reset_n) begin
+            if (!reset_n)
+                iaddr_rollback <= '0;
+            else if (bp_taken)
+                iaddr_rollback <= (iaddr_hold) ? instruction_address : next_instruction_address;
+        end
+
+        always_comb begin
+            if (ctx_switch_i)
+                iaddr_jumped = ctx_switch_target_i;
+            else if (jump_i)
+                iaddr_jumped = jump_target_i;
+            else if (jump_rollback_i)
+                iaddr_jumped = iaddr_rollback;
+            else
+                iaddr_jumped = bp_target_i;
+        end
+
+        always_ff @(posedge clk or negedge reset_n) begin
+            if (!reset_n)
+                bp_ack_o <= '0;
+            else if (sys_reset)
+                bp_ack_o <= '0;
+            else if (enable_i)
+                bp_ack_o <= bp_taken;
+        end
+
+        assign push = valid_fetch && !(enable_i && !valid_buffer);
+
+        assign bp_rollback_o  = 1'b0;
+    end
+    else begin : gen_bp_off
+        assign push           = !busy_r && !(enable_i && !valid_buffer);
+        assign jumped         = ctx_switch_i || jump_i;
+        assign jump_confirmed = jumped;
+        assign bp_ack_o       = 1'b0;
+        assign bp_rollback_o  = 1'b0;
+
+        always_comb begin
+            if (ctx_switch_i)
+                iaddr_jumped = ctx_switch_target_i;
+            else
+                iaddr_jumped = jump_target_i;
+        end
+    end
+
+    assign iaddr_hold = (almost_full && !pop && !busy_r) || (!pop && !mem_operation_en && busy_r) || busy_i;
 
     logic iaddr_hold_r;
     always_ff @(posedge clk or negedge reset_n) begin
@@ -142,7 +216,8 @@ module fetch
             iaddr_hold_r <= iaddr_hold;
     end
 
-    logic [31:0] instruction_address;
+    assign next_instruction_address = instruction_address + 32'd4;
+
     always_ff @(posedge clk or negedge reset_n) begin
         if (!reset_n) begin
             instruction_address <= start_address;
@@ -151,10 +226,10 @@ module fetch
             instruction_address <= start_address;
         end
         else begin
-            if (jumped)
+            if (jumped || jump_rollback_i)
                 instruction_address <= iaddr_jumped;
             else if (!iaddr_hold || (iaddr_hold_r && push && mem_operation_en))
-                instruction_address <= instruction_address + 32'd4;
+                instruction_address <= next_instruction_address;
         end
     end
 
@@ -164,8 +239,7 @@ module fetch
 // CPU Interface
 ////////////////////////////////////////////////////////////////////////////////
 
-    logic valid;
-    assign valid = valid_buffer || !busy_r;
+    assign valid = valid_fetch || valid_buffer;
 
     always_ff @(posedge clk or negedge reset_n) begin
         if (!reset_n)
@@ -189,15 +263,18 @@ module fetch
     end
 
     logic [31:0] pc_jumped;
-    logic jumped_r;
+    
     always_ff @(posedge clk or negedge reset_n) begin
         if (!reset_n) 
             pc_jumped <= start_address;
         else if (sys_reset)
             pc_jumped <= start_address;
-        else if (jumped_r)
-            pc_jumped <= instruction_address;
+        else if (jumped)
+            pc_jumped <= iaddr_jumped;
     end
+
+    logic [31:0] next_pc;
+    assign next_pc = pc_o + 32'd4;
 
     always_ff @(posedge clk or negedge reset_n) begin
         if (!reset_n) begin
@@ -207,10 +284,11 @@ module fetch
             pc_o <= start_address;
         end
         else begin
-            if (jumping_o)
+            if (jumping_o && !jump_rollback_i)
                 pc_o <= pc_jumped;
-            else if (enable_i && valid)
-                pc_o <= pc_o + 32'd4;
+            // else if (enable_i && valid && !bp_rollback_o)
+            else if (enable_i && valid_o)
+                pc_o <= next_pc;
         end
     end
 
@@ -222,10 +300,11 @@ module fetch
             jumped_r <= 1'b1;
         end
         else begin
-            if (jumped)
-                jumped_r <= 1'b1;
-            else if (enable_i)
-                jumped_r <= 1'b0;
+            jumped_r <= jumped;
+            // if (jumped)
+            //     jumped_r <= 1'b1;
+            // else if (enable_i || jump_rollback_i)
+            //     jumped_r <= 1'b0;
         end
     end
     
@@ -237,15 +316,17 @@ module fetch
             jumping_o <= 1'b1;
         end
         else begin 
-            if (jumped || jumped_r)
+            // if (jumped || (jumped_r && !jump_rollback_i))
+            if (jumped)
                 jumping_o <= 1'b1;
-            else if (enable_i)
+            // else if ((enable_i && valid) || jump_rollback_i)
+            // else if ((enable_i && valid) || jump_rollback_i)
+            else if (enable_i || jump_rollback_i)
+            // else if ((enable_i && valid) || jump_rollback_i || (jump_rollback_r && valid))
                 jumping_o <= 1'b0;
         end
     end
 
-    assign bp_ack_o = 1'b0;
-    assign bp_rollback_o = 1'b0;
     assign jump_misaligned_o = 1'b0;
     assign compressed_o = 1'b0;
 
@@ -261,59 +342,9 @@ module fetch
 // PC control
 ////////////////////////////////////////////////////////////////////////////////
 
-    // logic [31:0] pc_update;
-    // logic [31:0] pc_jumped;
-
-    // logic [31:0] pc_jumped_r;
-    // always_ff @(posedge clk or negedge reset_n) begin
-    //     if (!reset_n)
-    //         pc_jumped_r <= '0;
-    //     else if (enable_i)
-    //         pc_jumped_r <= pc_jumped;
-    // end
-
-    // /* pc_o is the PC of the fetched instruction */
-    // /* at the moment the instruction arrives     */
-    // always_ff @(posedge clk or negedge reset_n) begin
-    //     if (!reset_n)
-    //         pc_o <= '0;
-    //     else if (enable_i && !is_invalid && !jump_misaligned_o) 
-    //         pc_o <= pc_update;
-    // end
-
 ////////////////////////////////////////////////////////////////////////////////
 // Data control
 ////////////////////////////////////////////////////////////////////////////////
-
-    // logic fetched;
-    // always_ff @(posedge clk or negedge reset_n) begin
-    //     if (!reset_n)
-    //         fetched <= '0;
-    //     else
-    //         fetched <= enable_i && !busy_i;
-    // end
-
-    // logic [31:0] idata_held;
-    // always_ff @(posedge clk or negedge reset_n) begin
-    //     if (!reset_n)
-    //         idata_held <= 32'h00000013;
-    //     else if ((!enable_i || busy_i) && fetched)
-    //         idata_held <= instruction_data_i;
-    // end
-
-    // logic [31:0] instruction_fetched;
-    // assign instruction_fetched = fetched ? instruction_data_i : idata_held;
-
-    // logic [31:0] instruction_next;
-
-    // always_ff @(posedge clk or negedge reset_n) begin
-    //     if (!reset_n)
-    //         instruction_o <= 32'h00000013;
-    //     else if (sys_reset)
-    //         instruction_o <= 32'h00000013;
-    //     else if (enable_i && !is_invalid)
-    //         instruction_o <= instruction_next;
-    // end
 
 ////////////////////////////////////////////////////////////////////////////////
 // Branch prediction
@@ -331,39 +362,6 @@ module fetch
     //             iaddr_fetched_adv <= '0;
     //         else if ((enable_i && !busy_i) && bp_take_i)
     //             iaddr_fetched_adv <= iaddr_advance;
-    //     end
-
-    //     always_ff @(posedge clk or negedge reset_n) begin
-    //         if (!reset_n) begin
-    //             jump_rollback_r <= 1'b0;
-    //         end
-    //         else if (sys_reset) begin
-    //             jump_rollback_r <= 1'b0;
-    //         end
-    //         else begin 
-    //             if (jump_rollback_i)
-    //                 jump_rollback_r <= 1'b1;
-    //             else if (enable_i)
-    //                 jump_rollback_r <= 1'b0;
-    //         end
-    //     end
-
-    //     always_ff @(posedge clk or negedge reset_n) begin
-    //         if (!reset_n)
-    //             bp_rollback_o <= 1'b0;
-    //         else if (sys_reset)
-    //             bp_rollback_o <= 1'b0;
-    //         else if (enable_i)
-    //             bp_rollback_o <= jump_rollback_r;
-    //     end
-
-    //     assign jumped = ctx_switch_i || jump_i || (enable_i && !busy_i && bp_take_i);
-
-    //     always_ff @(posedge clk or negedge reset_n) begin
-    //         if (!reset_n)
-    //             bp_ack_o <= '0;
-    //         else if (enable_i)
-    //             bp_ack_o <= bp_take_i && !busy_i;
     //     end
     // end
     // else begin : gen_bp_off
