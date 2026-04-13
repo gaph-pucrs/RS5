809479ae62b98ce1d04913fefd766c18a2cff131
Lucas Damo <lucas.damo@edu.pucrs.br>
2026-04-12 22:52:21 -0300

    update uarts

--- proto/UART_RX_CTRL.sv
+++ proto/UART_RX_CTRL.sv
@@ -1,137 +1,121 @@
-module UART_RX_CTRL 
-    import RS5_pkg::*;
-  #(parameter CLKS_PER_BIT = 868)
-  (
-   input logic    i_Clock,
-   input logic    i_Rx_Serial,
-   output logic   o_Rx_DV,
-   output logic [7:0] o_Rx_Byte
-   );
-    
-  localparam s_IDLE         = 3'b000;
-  localparam s_RX_START_BIT = 3'b001;
-  localparam s_RX_DATA_BITS = 3'b010;
-  localparam s_RX_STOP_BIT  = 3'b011;
-  localparam s_CLEANUP      = 3'b100;
-   
-  logic           r_Rx_Data_R = 1'b1;
-  logic           r_Rx_Data   = 1'b1;
-   
-  logic [14:0]     r_Clock_Count = 0;
-  logic [2:0]     r_Bit_Index   = 0; //8 bits total
-  logic [7:0]     r_Rx_Byte     = 0;
-  logic           r_Rx_DV       = 0;
-  logic [2:0]     r_SM_Main     = 0;
-   
-  // Purpose: Double-logicister the incoming data.
-  // This allows it to be used in the UART RX Clock Domain.
-  // (It removes problems caused by metastability)
-  always @(posedge i_Clock)
-    begin
-      r_Rx_Data_R <= i_Rx_Serial;
-      r_Rx_Data   <= r_Rx_Data_R;
-    end
-   
-   
-  // Purpose: Control RX state machine
-  always @(posedge i_Clock)
-    begin
-       
-      case (r_SM_Main)
-        s_IDLE :
-          begin
-            r_Rx_DV       <= 1'b0;
-            r_Clock_Count <= 0;
-            r_Bit_Index   <= 0;
-             
-            if (r_Rx_Data == 1'b0)          // Start bit detected
-              r_SM_Main <= s_RX_START_BIT;
-            else
-              r_SM_Main <= s_IDLE;
-          end
-         
-        // Check middle of start bit to make sure it's still low
-        s_RX_START_BIT :
-          begin
-            if (r_Clock_Count == (CLKS_PER_BIT-1)/2)
-              begin
-                if (r_Rx_Data == 1'b0)
-                  begin
-                    r_Clock_Count <= 0;  // reset counter, found the middle
-                    r_SM_Main     <= s_RX_DATA_BITS;
-                  end
-                else
-                  r_SM_Main <= s_IDLE;
-              end
-            else
-              begin
-                r_Clock_Count <= r_Clock_Count + 1;
-                r_SM_Main     <= s_RX_START_BIT;
-              end
-          end // case: s_RX_START_BIT
-         
-         
-        // Wait CLKS_PER_BIT-1 clock cycles to sample serial data
-        s_RX_DATA_BITS :
-          begin
-            if (r_Clock_Count < CLKS_PER_BIT-1)
-              begin
-                r_Clock_Count <= r_Clock_Count + 1;
-                r_SM_Main     <= s_RX_DATA_BITS;
-              end
-            else
-              begin
-                r_Clock_Count          <= 0;
-                r_Rx_Byte[r_Bit_Index] <= r_Rx_Data;
-                 
-                // Check if we have received all bits
-                if (r_Bit_Index < 7)
-                  begin
-                    r_Bit_Index <= r_Bit_Index + 1;
-                    r_SM_Main   <= s_RX_DATA_BITS;
-                  end
-                else
-                  begin
-                    r_Bit_Index <= 0;
-                    r_SM_Main   <= s_RX_STOP_BIT;
-                  end
-              end
-          end // case: s_RX_DATA_BITS
-     
-     
-        // Receive Stop bit.  Stop bit = 1
-        s_RX_STOP_BIT :
-          begin
-            // Wait CLKS_PER_BIT-1 clock cycles for Stop bit to finish
-            if (r_Clock_Count < CLKS_PER_BIT-1)
-              begin
-                r_Clock_Count <= r_Clock_Count + 1;
-                r_SM_Main     <= s_RX_STOP_BIT;
-              end
-            else
-              begin
-                r_Rx_DV       <= 1'b1;
-                r_Clock_Count <= 0;
-                r_SM_Main     <= s_CLEANUP;
-              end
-          end // case: s_RX_STOP_BIT
-     
-         
-        // Stay here 1 clock
-        s_CLEANUP :
-          begin
-            r_SM_Main <= s_IDLE;
-            r_Rx_DV   <= 1'b0;
-          end
-         
-         
-        default :
-          r_SM_Main <= s_IDLE;
-         
-      endcase
-    end   
-   
-  assign o_Rx_DV   = r_Rx_DV;
-  assign o_Rx_Byte = r_Rx_Byte;
-   
-endmodule // uart_rx
+module UART_RX_CTRL
+#(
+	parameter int unsigned CLK_FREQUENCE = 256_000_000,
+ 	parameter int unsigned BAUD_RATE = 115_200
+)
+(
+	input  logic         clk,
+	input  logic         reset_n,
+	input  logic     	 UART_RX_serial_i,
+	output logic         UART_byte_valid_o,
+	output logic [7:0]   UART_byte_o,
+	input  logic [15:0]	 clks_per_bit_i
+);
+
+	typedef enum logic[2:0] { 
+		RX_START_BIT = 3'b001,
+		RX_DATA_BITS = 3'b010,
+		RX_STOP_BIT  = 3'b100
+	} state_e;
+
+	logic r_rx_serial;
+	
+	// Purpose: Double-logicister the incoming data.
+	// This allows it to be used in the UART RX Clock Domain.
+	// (It removes problems caused by metastability)
+	
+	prim_sync #(
+        .RESET_VALUE(1'b1)
+	) u_serial_sync (
+        .clk(clk),
+        .reset_n(reset_n),
+        .d(UART_RX_serial_i),
+        .q(r_rx_serial)
+    );
+
+	state_e current_state;
+	state_e next_state;
+
+	always_ff @(posedge clk or negedge reset_n) begin 
+		if (!reset_n) 
+			current_state <= RX_START_BIT;
+		else
+			current_state <= next_state;
+	end
+
+	logic [15:0] r_Clock_Count;
+	logic [15:0] clks_per_bit;
+
+	logic clk_cnt_top;
+	assign clk_cnt_top = (r_Clock_Count >= clks_per_bit);
+	
+	always_ff @(posedge clk or negedge reset_n) begin
+		if (!reset_n) begin
+			r_Clock_Count <= '0;
+		end 
+		else begin
+			if ((current_state == RX_START_BIT && r_rx_serial) || clk_cnt_top) begin
+				r_Clock_Count <= '0;
+			end
+			else begin
+				r_Clock_Count <= r_Clock_Count + 1'b1;
+			end
+		end
+	end
+
+	logic clk_cnt_half;
+	assign clk_cnt_half = (r_Clock_Count == {1'b0, clks_per_bit[15:1]});
+
+	logic [2:0] r_Bit_Index;
+	always_ff @(posedge clk or negedge reset_n) begin
+		if (!reset_n)
+			r_Bit_Index <= '0;
+		else if (clk_cnt_half && current_state == RX_DATA_BITS)
+			r_Bit_Index <= r_Bit_Index + 1'b1;
+	end
+
+	logic [7:0] r_Rx_Byte;
+	always_ff @(posedge clk or negedge reset_n) begin
+		if (!reset_n)
+			r_Rx_Byte <= '0;
+		else if (clk_cnt_half && current_state == RX_DATA_BITS)
+			r_Rx_Byte[r_Bit_Index] <= r_rx_serial;
+	end
+	
+	always_comb begin
+		next_state = RX_START_BIT;
+		unique case(current_state)
+			RX_START_BIT: next_state = !r_rx_serial && clk_cnt_half        ? RX_DATA_BITS : RX_START_BIT;
+			RX_DATA_BITS: next_state = clk_cnt_half && r_Bit_Index == 3'd7 ? RX_STOP_BIT  : RX_DATA_BITS;
+			RX_STOP_BIT:  next_state = clk_cnt_half 					   ? RX_START_BIT : RX_STOP_BIT;
+			default: ;
+		endcase
+	end
+
+	logic r_Rx_DV;
+	always_ff @(posedge clk or negedge reset_n) begin: r_Rx_DV_fsm
+		if (!reset_n) begin
+			r_Rx_DV <= 1'b0;
+		end 
+		else begin
+			if (clk_cnt_half && current_state == RX_STOP_BIT)
+				r_Rx_DV <= 1'b1;
+			else
+				r_Rx_DV <= 1'b0;
+		end
+	end
+
+	always_ff @(posedge clk or negedge reset_n) begin
+		if (!reset_n) begin
+			clks_per_bit <= CLK_FREQUENCE/BAUD_RATE;
+		end 
+		else begin
+			if (current_state == RX_START_BIT)
+				clks_per_bit <= clks_per_bit_i;
+		end
+	end
+
+	assign UART_byte_valid_o = r_Rx_DV;
+	assign UART_byte_o 		 = r_Rx_Byte;
+	
+endmodule
