93c245ceed87d473817ffb131c9ab983cde6e952
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2026-05-01 15:56:43 -0300

    Add parameterizable cache modules

--- sim/testbench.sv
+++ sim/testbench.sv
@@ -64,6 +64,13 @@ module testbench
     localparam int           MEM_ADDR_BITS   = 28;
     localparam string        BIN_FILE        = "../app/riscv-tests/test.bin";
 
+    localparam bit           ICACHE_EN       = 1'b0;
+    localparam int           ICACHE_WIDTH    = 12;
+    localparam int           ICACHE_OFF_W    = 6;
+    localparam bit           DCACHE_EN       = 1'b0;
+    localparam int           DCACHE_WIDTH    = 12;
+    localparam int           DCACHE_OFF_W    = 6;
+
     localparam int           i_cnt = 1;
 
 ///////////////////////////////////////// Clock generator //////////////////////////////
@@ -121,6 +128,8 @@ module testbench
 // Control
 //////////////////////////////////////////////////////////////////////////////
 
+    logic [31:0] dmem_dataR;
+
     assign enable_ram  = mem_operation_enable && ((mem_address[31:28] == 4'b0000) || (mem_address[31:28] == 4'b0001));
     assign enable_rtc  = mem_operation_enable && (mem_address[31:28] == 4'b0010);
     assign enable_plic = mem_operation_enable && (mem_address[31:28] == 4'b0100);
@@ -137,7 +146,7 @@ module testbench
             3'b100:  mem_data_read = data_tb;
             3'b010:  mem_data_read = {{(BUS_WIDTH-32){1'b0}}, data_plic};
             3'b001:  mem_data_read = {{(BUS_WIDTH-32){1'b0}}, data_rtc[31:0]};
-            default: mem_data_read = data_ram;
+            default: mem_data_read = dmem_dataR;
         endcase
     end
 
@@ -192,6 +201,150 @@ module testbench
     );
 
 //////////////////////////////////////////////////////////////////////////////
+// Cache
+//////////////////////////////////////////////////////////////////////////////
+
+    logic        imem_busy;
+    logic        imem_ce;
+    logic [31:0] imem_data;
+
+    logic        dmem_busy;
+    logic        dmem_ce;
+    logic [3:0]  dmem_we;
+    logic [31:0] dmem_dataW;
+
+    /* Number of used bits is defined by the memory size */
+    /* verilator lint_off UNUSEDSIGNAL */
+    logic [31:0] imem_addr;
+    logic [31:0] dmem_addr;
+    /* verilator lint_on UNUSEDSIGNAL */
+
+    if (ICACHE_EN) begin : gen_icache_on
+        logic                    icache_ce;
+        logic [3:0]              icache_we;
+        logic [ICACHE_WIDTH-1:0] icache_addr;
+        logic [31:0]             icache_dataR;
+        logic [31:0]             icache_dataW;
+
+        DMWTCtrl #(
+            .ADDR_WIDTH  (MEM_ADDR_BITS),
+            .CACHE_WIDTH (ICACHE_WIDTH ),
+            .OFFSET_WIDTH(ICACHE_OFF_W )
+        ) icache_ctrl (
+            .clk         (clk                ),
+            .rst_n       (reset_n            ),
+            .ce_i        (enable_imem        ),
+            .we_i        ('0                 ),
+            .address_i   (instruction_address[MEM_ADDR_BITS-1:0]),
+            .data_i      ('0                 ),
+            .data_o      (instruction        ),
+            .busy_o      (busy               ),
+            .cache_ce_o  (icache_ce          ),
+            .cache_we_o  (icache_we          ),
+            .cache_addr_o(icache_addr        ),
+            .cache_data_i(icache_dataR       ),
+            .cache_data_o(icache_dataW       ),
+            .mem_ce_o    (imem_ce            ),
+            .mem_addr_o  (imem_addr[MEM_ADDR_BITS-1:0]),
+            .mem_data_i  (imem_data          ),
+            .mem_busy_i  (imem_busy          ),
+            /* verilator lint_off PINCONNECTEMPTY */
+            .mem_we_o    (/* Unconnected */  ),
+            .mem_data_o  (/* Unconnected */  )
+            /* verilator lint_on PINCONNECTEMPTY */
+        );
+
+        RAM_mem #(
+            .MEM_WIDTH(1 << ICACHE_WIDTH),
+            .BIN_FILE("")
+        ) icache_sram (
+            .clk    (clk),
+            .enA_i  (icache_ce),
+            .weA_i  (icache_we),
+            .addrA_i(icache_addr),
+            .dataA_i(icache_dataW),
+            .dataA_o(icache_dataR),
+            .enB_i  (1'b0),
+            .weB_i  ('0),
+            .addrB_i('0),
+            .dataB_i('0),
+            /* verilator lint_off PINCONNECTEMPTY */
+            .dataB_o(/* Unconnected */)
+            /* verilator lint_on PINCONNECTEMPTY */
+        );
+    end
+    else begin : gen_icache_off
+        assign imem_ce     = enable_imem;
+        assign imem_addr   = instruction_address;
+        assign instruction = imem_data;
+        assign busy        = imem_busy;
+    end
+
+    if (DCACHE_EN) begin : gen_dcache_on
+        logic                    dcache_ce;
+        logic [3:0]              dcache_we;
+        logic [ICACHE_WIDTH-1:0] dcache_addr;
+        logic [31:0]             dcache_dataR;
+        logic [31:0]             dcache_dataW;
+
+        logic dcache_busy;
+        assign stall = dcache_busy && enable_ram;
+
+        DMWTCtrl #(
+            .ADDR_WIDTH  (MEM_ADDR_BITS),
+            .CACHE_WIDTH (DCACHE_WIDTH ),
+            .OFFSET_WIDTH(DCACHE_OFF_W )
+        ) dcache_ctrl (
+            .clk         (clk                ),
+            .rst_n       (reset_n            ),
+            .ce_i        (enable_ram         ),
+            .we_i        (mem_write_enable   ),
+            .address_i   (mem_address[MEM_ADDR_BITS-1:0]),
+            .data_i      (mem_data_write     ),
+            .data_o      (dmem_dataR         ),
+            .busy_o      (dcache_busy        ),
+            .cache_ce_o  (dcache_ce          ),
+            .cache_we_o  (dcache_we          ),
+            .cache_addr_o(dcache_addr        ),
+            .cache_data_i(dcache_dataR       ),
+            .cache_data_o(dcache_dataW       ),
+            .mem_ce_o    (dmem_ce            ),
+            .mem_addr_o  (dmem_addr[MEM_ADDR_BITS-1:0]),
+            .mem_data_i  (data_ram           ),
+            .mem_busy_i  (dmem_busy          ),
+            .mem_we_o    (dmem_we            ),
+            .mem_data_o  (dmem_dataW         )
+        );
+
+        RAM_mem #(
+            .MEM_WIDTH(1 << DCACHE_WIDTH),
+            .BIN_FILE("")
+        ) dcache_sram (
+            .clk    (clk),
+            .enA_i  (dcache_ce),
+            .weA_i  (dcache_we),
+            .addrA_i(dcache_addr),
+            .dataA_i(dcache_dataW),
+            .dataA_o(dcache_dataR),
+            .enB_i  (1'b0),
+            .weB_i  ('0),
+            .addrB_i('0),
+            .dataB_i('0),
+            /* verilator lint_off PINCONNECTEMPTY */
+            .dataB_o(/* Unconnected */)
+            /* verilator lint_on PINCONNECTEMPTY */
+        );
+    end
+    else begin : gen_dcache_off
+        assign dmem_ce    = enable_ram;
+        assign dmem_we    = mem_write_enable;
+        assign dmem_addr  = mem_address;
+        assign dmem_dataW = mem_data_write;
+        assign dmem_dataR = data_ram;
+        assign stall      = dmem_busy;
+    end
+
+//////////////////////////////////////////////////////////////////////////////
 // RAM
 //////////////////////////////////////////////////////////////////////////////
 
@@ -237,50 +390,50 @@ module testbench
 
     logic enable_ram_delayed;
     initial begin
-        stall = 1'b0;
+        dmem_busy = 1'b0;
         enable_ram_delayed = 1'b0;
 
         forever begin
             // 1. Wait for enable_ram to go high
-            @(negedge clk iff enable_ram);
+            @(negedge clk iff dmem_ce);
 
             // 2. Assert stall
-            stall = 1'b1;
+            dmem_busy = 1'b1;
 
             // 3. Wait for some cycles (to simulate delay)
             repeat (RAM_DELAY_CYCLES) @(negedge clk);
 
             // 4. Deassert stall, assert delayed signal
-            stall = 1'b0;
+            dmem_busy = 1'b0;
             enable_ram_delayed = 1'b1;
 
             // 5. Wait for enable_ram to go low to finish
-            @(negedge clk iff !enable_ram);
+            @(negedge clk iff !dmem_ce);
             enable_ram_delayed = 1'b0;
         end
     end
 
     if (DUALPORT_MEM) begin : dual_port
-        assign enA         = enable_imem;
+        assign enA         = imem_ce;
         assign weA         = '0;
-        assign addrA       = instruction_address[($clog2(MEM_WIDTH) - 1):0];
+        assign addrA       = imem_addr[($clog2(MEM_WIDTH) - 1):0];
         assign dataAi      = '0;
-        assign instruction = dataAo;
+        assign imem_data   = dataAo;
 
         assign enB         = enable_ram_delayed;
-        assign weB         = mem_write_enable;
-        assign addrB       = mem_address[($clog2(MEM_WIDTH) - 1):0];
-        assign dataBi      = mem_data_write;
+        assign weB         = dmem_we;
+        assign addrB       = dmem_addr[($clog2(MEM_WIDTH) - 1):0];
+        assign dataBi      = dmem_dataW;
         assign data_ram    = dataBo;
 
-        assign busy        = 1'b0;
+        assign imem_busy   = 1'b0;
     end
     else begin : single_port
-        assign enA         = enable_imem || enable_ram_delayed;
-        assign weA         = enable_ram_delayed ? mem_write_enable : '0;
-        assign addrA       = enable_ram_delayed ? mem_address[($clog2(MEM_WIDTH) - 1):0] : instruction_address[($clog2(MEM_WIDTH) - 1):0];
-        assign dataAi      = mem_data_write;
-        assign instruction = dataAo;
+        assign enA         = imem_ce || enable_ram_delayed;
+        assign weA         = enable_ram_delayed ? dmem_we : '0;
+        assign addrA       = enable_ram_delayed ? dmem_addr[($clog2(MEM_WIDTH) - 1):0] : imem_addr[($clog2(MEM_WIDTH) - 1):0];
+        assign dataAi      = dmem_dataW;
+        assign imem_data   = dataAo;
         assign data_ram    = dataAo;
 
         assign enB         = '0;
@@ -288,7 +441,7 @@ module testbench
         assign addrB       = '0;
         assign dataBi      = '0;
 
-        assign busy        = enable_ram;
+        assign imem_busy   = dmem_ce;
     end
 
 //////////////////////////////////////////////////////////////////////////////
