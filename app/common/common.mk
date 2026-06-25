b480caec05ab0f913d866d99ccf82fb043f99625
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2026-06-25 16:33:41 -0300

    Automatically rebuild on arch/mem size change for common libraries

--- app/common/common.mk
+++ app/common/common.mk
@@ -1,51 +1,49 @@
-RED  =\033[0;31m
-NC   =\033[0m # No Color
+################################################################################
+## Configurable parameters
+################################################################################
 
-COMMON_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-
-SHA256_ZKNH ?= 1
-
-SHA512_ZKNH ?= 1
-
-AES_ASM ?= 0
-
-KYBER_ISE ?= 0
-
-ifneq (${SHA256_ZKNH}, 0)
-	DEFS += -DSHA256_ZKNH
-endif
-
-ifneq (${SHA512_ZKNH}, 0)
-	DEFS += -DSHA512_ZKNH
-endif
-
-ifneq (${AES_ASM}, 0)
-	DEFS += -DAES_RISCV_ASM
-endif
-
-ifneq (${KYBER_ISE}, 0)
-	DEFS += -DKYBER_ISE
-endif
-
-DEFS += -DTC_AES_256
-
-# DEFS += -DRVKINTRIN_ASSEMBLER
-DEFS += -DRVKINTRIN_EMULATE
-
-DEFS += -DRVK_ALGTEST_VERBOSE_SIO
-
-TARGET   ?= $(notdir $(CURDIR))
 MEM_SIZE ?= 65536
 ARCH     ?= rv32im_zicsr_zkne_zknh
-#ARCH 	  =	rv32imc_zicsr_zkne_zknh_zbkb
+
+################################################################################
+## Build rules
+################################################################################
+
+RED  =\033[0;31m
+NC   =\033[0m # No Color
+TARGET   ?= $(notdir $(CURDIR))
 
 CC 		= riscv64-elf-gcc
 OBJDUMP = riscv64-elf-objdump
 OBJCOPY = riscv64-elf-objcopy
 
+COMMON_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+ARCH_LOWER := $(shell echo $(ARCH) | tr '[:upper:]' '[:lower:]')
+ARCH_STAMP     := $(COMMON_DIR)/.arch_stamp
+MEM_SIZE_STAMP := $(COMMON_DIR)/.mem_size_stamp
+$(shell \
+  if [ "$$(cat $(ARCH_STAMP) 2>/dev/null)" != "$(ARCH_LOWER)" ]; then \
+    echo "$(ARCH_LOWER)" > $(ARCH_STAMP) && \
+    printf "$(RED)ARCH stamp updated: $(ARCH_LOWER)$(NC)\n" >&2; \
+  fi)
+$(shell \
+  if [ "$$(cat $(MEM_SIZE_STAMP) 2>/dev/null)" != "$(MEM_SIZE)" ]; then \
+    echo "$(MEM_SIZE)" > $(MEM_SIZE_STAMP) && \
+    printf "$(RED)MEM_SIZE stamp updated: $(MEM_SIZE)$(NC)\n" >&2; \
+  fi)
+
 SRCDIR  = src
 INCDIR  = $(SRCDIR)/include
 HEADERS = $(wildcard $(INCDIR)/*.h) $(wildcard $(COMMON_DIR)/include/*.h) $(wildcard $(COMMON_DIR)/include/tinycrypt/*.h)
+
+DEFS += -DTC_AES_256 -DRVK_ALGTEST_VERBOSE_SIO
+
+# Zkne -> AES_RISCV_ASM; without -> don't define
+DEFS += $(if $(findstring zkne,$(ARCH_LOWER)), -DAES_RISCV_ASM)
+# Zknh -> SHA2 flags; without -> don't define
+DEFS += $(if $(findstring zknh,$(ARCH_LOWER)), -DSHA256_ZKNH -DSHA512_ZKNH)
+# Zbkb -> assembler; without -> emulate
+DEFS += $(if $(findstring zbkb,$(ARCH_LOWER)), -DRVKINTRIN_ASSEMBLER, -DRVKINTRIN_EMULATE)
 
 CFLAGS  = -march=$(ARCH) -mabi=ilp32 -Os -fdata-sections -ffunction-sections -Wall -std=c23 -I$(INCDIR) -I$(COMMON_DIR)/include -I$(COMMON_DIR)/include/tinycrypt $(DEFS)
 LDFLAGS = --specs=nano.specs -T $(COMMON_DIR)/link.ld -Wl,--gc-sections -march=$(ARCH) -mabi=ilp32 -nostartfiles -lm -u _printf_float
@@ -70,11 +68,11 @@ $(TARGET).elf: $(CCOBJ) $(ASOBJ)
 	@printf "${RED}Linking %s...${NC}\n" "$@"
 	@$(CC) $(CCOBJ) $(ASOBJ) -Wl,-Map=$(TARGET).map -N -o $@ $(LDFLAGS)
 
-%.o: %.S
+%.o: %.S $(ARCH_STAMP) $(MEM_SIZE_STAMP)
 	@printf "${RED}Assemblying %s...${NC}\n" "$<"
 	@$(CC) -c $< -o $@ $(CFLAGS) -march=$(ARCH) -DMEM_SIZE=$(MEM_SIZE)
 
-%.o: %.c $(HEADERS)
+%.o: %.c $(HEADERS) $(ARCH_STAMP)
 	@printf "${RED}Compiling %s...${NC}\n" "$<"
 	@$(CC) -c $< -o $@ $(CFLAGS)
 
@@ -89,5 +87,6 @@ clean:
 	@rm -rf *.map
 	@rm -rf *.lst
 	@rm -rf *.elf
+	@rm -f $(COMMON_DIR)/$(ARCH_STAMP) $(COMMON_DIR)/$(MEM_SIZE_STAMP)
 
 .PHONY: clean
